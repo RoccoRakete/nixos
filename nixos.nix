89b037822a607c5e3c1cca560f7ee0dce070e6bc
RoccoRakete <m.schaumann@mscloud.uk>
2023-11-30 18:29:27 +0100

    ...

--- nixos.nix
+++ nixos.nix
@@ -9,7 +9,7 @@ let
 in
 
 {
-  
+
   nix = {
     optimise.automatic = true;
     gc = {
@@ -108,7 +108,25 @@ in
     };
     printing.enable = true;
     flatpak.enable = true;
-
+   # thinkfan.enable = true;
+   # thinkfan.sensors = [
+   #   {
+   #     query = "/proc/acpi/ibm/thermal";
+   #     type = "tpacpi";
+   #   }
+   # ];
+   # thinkfan.fans = [
+   #   {
+   #     query = "/proc/acpi/ibm/fan";
+   #     type = "tpacpi";
+   #   }
+   # ];
+   # thinkfan.levels = [
+   #   [ 0 0 85 ]
+   #   [ 1 82 91 ]
+   #   [ "level auto" 89 95 ]
+   #   [ "level disengaged" 94255 ]
+   # ];
   };
 
   hardware = {
